a8139f466459749c71914074d41acbbc853ef03a
Mark Robinson <mark.robinson@mls.uzh.ch>
2026-06-20 16:28:49 +0200

    pull out wrong arg

--- pca.R
+++ pca.R
@@ -94,7 +94,7 @@ main <- function() {
 
   dir.create(args$output_dir, showWarnings = FALSE, recursive = TRUE)
 
-  m <- TENxMatrix(args$input_h5, group = "matrix")
+  m <- TENxMatrix(args$normalized_selected.h5, group = "matrix")
   m <- as(m, "dgCMatrix")
   cat(sprintf("  matrix (genes x cells): %d x %d\n", nrow(m), ncol(m)))
 
