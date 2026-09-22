7784c98ce8b769cee77245d8e65719c8ba5d457f
Mark Robinson <mark.robinson@mls.uzh.ch>
2026-09-22 17:13:34 +0200

    pull out vector

--- filter.R
+++ filter.R
@@ -44,7 +44,7 @@ sce <- read_h5ad(args$rawdata_h5ad, as = "SingleCellExperiment")
 # read input H5AD
 props <- read_yaml(args$properties_info)
 cat(sprintf("LOG: props: \n%s\n", capture.output(print(props))))
-batch <- props$batch_var
+batch <- sce[[props$batch_var]]
 
 # do sample-wise filtering scrapper-style
 is.mito <- grepl("^[Mm][Tt]-", rownames(sce))
