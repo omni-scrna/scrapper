ce1bc1473ddcf0af420e3a2b4c2927bd7ea2ed40
Mark Robinson <mark.robinson@mls.uzh.ch>
2026-09-22 17:05:08 +0200

    add more log

--- filter.R
+++ filter.R
@@ -43,6 +43,7 @@ sce <- read_h5ad(args$rawdata_h5ad, as = "SingleCellExperiment")
 
 # read input H5AD
 props <- read_yaml(args$properties_info)
+cat(sprintf("LOG: props: \n%s\n", capture.output(print(props))))
 batch <- props$batch_var
 
 # do sample-wise filtering scrapper-style
